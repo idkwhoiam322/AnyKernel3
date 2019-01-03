715cede475fb0f591c567184f6460977cd110666
idkwhoiam322 <snkm1999@gmail.com>
2019-01-03 16:22:53 +0300

    semi-pissel setup i've been trying for a bit

--- ramdisk/init.weeb.sh
+++ ramdisk/init.weeb.sh
@@ -14,9 +14,9 @@ sleep 25;
 	echo 1 > /sys/devices/system/cpu/cpu4/cpufreq/schedutil/iowait_boost_enable
 
 # Input boost and stune configuration
-	echo "0:1036800 1:0 2:0 3:0 4:0 5:0 6:0 7:0" > /sys/module/cpu_boost/parameters/input_boost_freq
-	echo 450 > /sys/module/cpu_boost/parameters/input_boost_ms
-	echo 0 > /sys/module/cpu_boost/parameters/dynamic_stune_boost
+	echo "0:1036800 1:0 2:0 3:0 4:1056000 5:0 6:0 7:0" > /sys/module/cpu_boost/parameters/input_boost_freq
+	echo 1000 > /sys/module/cpu_boost/parameters/input_boost_ms
+	echo 30 > /sys/module/cpu_boost/parameters/dynamic_stune_boost
 
 # Set min cpu freq
 	echo 518400 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
@@ -26,12 +26,12 @@ sleep 25;
 	echo Y > /sys/module/workqueue/parameters/power_efficient
 
 # Disable Touchboost
-	echo 1 > /sys/module/msm_performance/parameters/touchboost
+	echo 0 > /sys/module/msm_performance/parameters/touchboost
 
 # Set default schedTune value for foreground/top-app
 	echo 1 > /dev/stune/foreground/schedtune.prefer_idle
-	echo 1 > /dev/stune/top-app/schedtune.boost
-	echo 0 > /dev/stune/top-app/schedtune.sched_boost
+	echo 10 > /dev/stune/top-app/schedtune.boost
+	echo 30 > /dev/stune/top-app/schedtune.sched_boost
 	echo 1 > /dev/stune/top-app/schedtune.prefer_idle
 
 # Setup EAS cpusets values for better load balancing
