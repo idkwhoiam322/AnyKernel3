743106558fcdd537431d97e4bbcaaa443e974c07
idkwhoiam322 <snkm1999@gmail.com>
2019-05-08 11:31:22 +0300

    Only set the cpusets that actually exist

--- ramdisk/init.weeb.sh
+++ ramdisk/init.weeb.sh
@@ -17,14 +17,13 @@ sleep 35;
 	echo 1 > /dev/stune/top-app/schedtune.prefer_idle
 
 # kang pixel 3 cpusets
-	echo 0-1,4-5 > /dev/cpuset/application/cpus
+	echo 0-7 > /dev/cpuset/audio-app/cpus
 	echo 0-1 > /dev/cpuset/background/cpus
 	echo 0-7 > /dev/cpuset/camera-daemon/cpus
 	echo 0-3,6-7 > /dev/cpuset/foreground/cpus
-	echo 0-7 > /dev/cpuset/kernel/cpus
 	echo 0-3 > /dev/cpuset/restricted/cpus
-	echo 0-3 > /dev/cpuset/system-background/cpus
 	echo 0-1,6-7 > /dev/cpuset/system/cpus
+	echo 0-3 > /dev/cpuset/system-background/cpus
 	echo 0-7 > /dev/cpuset/top-app/cpus
 
 # Adjust LMK Values
