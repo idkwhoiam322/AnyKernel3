8f41355177049a9c5ef65eb123d3282a9bfb5b69
osm0sis <osm0sis@outlook.com>
2019-07-20 21:10:37 -0300

    Example: update to use set_perm_recursive DO NOT MERGE if you have already updated your ramdisk properly for your device

--- anykernel.sh
+++ anykernel.sh
@@ -38,8 +38,8 @@ fi;
 
 ## AnyKernel file attributes
 # set permissions/ownership for included ramdisk files
-chmod -R 750 $ramdisk/*;
-chown -R root:root $ramdisk/*;
+set_perm_recursive 0 0 755 644 $ramdisk/*;
+set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
 
 # Check if user is on OxygenOS or Cutom ROM
 userflavor="$(file_getprop /system/build.prop "ro.build.user"):$(file_getprop /system/build.prop "ro.build.flavor")";
