5a59becb1e16b19ffc4c8909185589d615fd0a74
idkwhoiam322 <idkwhoiam322@raphielgang.org>
2019-07-21 14:47:59 +0530

    anykernel: remove unused bit

--- anykernel.sh
+++ anykernel.sh
@@ -28,14 +28,6 @@ ramdisk_compression=auto;
 # import patching functions/variables - see for reference
 . tools/ak3-core.sh;
 
-# Save the users from themselves
-android_version="$(file_getprop /system/build.prop "ro.build.version.release")";
-supported_version=9;
-if [ "$android_version" != "$supported_version" ]; then
-  ui_print " "; ui_print "You are on $android_version but this kernel is only for $supported_version!";
-  exit 1;
-fi;
-
 ## AnyKernel file attributes
 # set permissions/ownership for included ramdisk files
 set_perm_recursive 0 0 755 644 $ramdisk/*;
